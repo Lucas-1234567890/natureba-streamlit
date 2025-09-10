f2e9e85a95b25d801bf4d5a0407417d2a029c666
Lucas Amorim <158764384+Lucas-1234567890@users.noreply.github.com>
2025-09-10 14:31:55 -0300

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -11,7 +11,9 @@ def sidebar_navegacao():
         "📊 Relatórios",
         "🔃 Produção",
         "📦 Estoque",
+        "💸 Custos Fixos",
         "⚙️ Configurações"
+
     ]
 
     escolha = st.sidebar.selectbox("Navegação", menu_de_opcoes)
